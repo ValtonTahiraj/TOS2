46d75f85ec36a2145d3dfff001414de8e92ce5da
ValtonTahiraj <tahirajvalton@gmail.com>
2020-11-28 20:27:33 +0100

    primo commit

---
+++ src/main/java/it/unipd/tos/App.java
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////////////////////////
+// [VALTON] [TAHIRAJ] [1193389]
+////////////////////////////////////////////////////////////////////
+
+package it.unipd.tos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
